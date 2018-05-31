c3396898853ca680292355de263d7b3c3c246aa9
Suvarna Khanal <suvarna.khanal@gmail.com>
2018-05-31 20:11:35 +0530

    main changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 int main(int argc, char* argv[])
 {
+	if(argc==1 and argv[0]=="lovec")
+	{
+		std::cerr<<"Usage: lovec [action]"<<std::endl;
+		return -1;
+	}	
 
-	
+	if(argv[1]=="create-repo")
+	{
+		//call create-repo
+	}
 
 	return 0;
 }
