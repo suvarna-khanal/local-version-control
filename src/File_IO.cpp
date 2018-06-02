70299df55af9fa19ee4d79ae9b82d7df32459aa7
Suvarna Khanal <suvarna.khanal@gmail.com>
2018-06-02 23:37:42 +0530

    this added

---
+++ src/File_IO.cpp
@@ -0,0 +1,28 @@
+#include "File_IO.h"
+
+File_IO::File_IO()
+{
+
+	//ctor
+}
+
+File_IO::~File_IO()
+{
+	//dtor
+	
+}
+
+int File_IO::file_write(const std::string& file_name, const std::string& contents)
+{
+	
+
+	return 0;
+
+}
+
+int File_IO::file_read(const std::string& file_name, const std::string& contents)
+{
+
+	return 0;
+
+}
